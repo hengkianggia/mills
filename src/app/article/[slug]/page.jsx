5d4cde6a5ff3aab47e53ede1b558afab0486007b
HengkiAnggia <pengkyjd00@gmail.com>
2024-03-02 13:00:25 +0700

    change npm to pnpm

--- src/app/article/[slug]/page.jsx
+++ src/app/article/[slug]/page.jsx
@@ -1,3 +1,5 @@
+import React from 'react'
+
 import Div from '@/components/helper/Div'
 import AboutUs from '@/components/pages/article/AboutUs'
 import ExchangeAndReturn from '@/components/pages/article/ExhangeAndReturn'
@@ -7,7 +9,6 @@ import PaymentMethods from '@/components/pages/article/PaymentMethods'
 import PrivacyPolicy from '@/components/pages/article/PrivacyPolicy'
 import ShippingMethods from '@/components/pages/article/ShippingMethods'
 import Title from '@/components/title component/Title'
-import React from 'react'
 import { FaFacebook, FaLinkedin, FaTelegram, FaTwitter, FaWhatsapp } from 'react-icons/fa'
 import { MdEmail } from 'react-icons/md'
 
@@ -17,18 +18,22 @@ let title = params.slug
   return (
     <>
       <Div wrap className="bg-gradient-to-b from-yellow-50 to-white px-4">
-        <Div wrap className="w-full max-w-screen-maxxx min-h-[80vh] md:min-h-[50vh] lg:min-h-[50vh]">
+        <Div
+          wrap
+          className="w-full max-w-screen-maxxx min-h-[80vh] md:min-h-[50vh] lg:min-h-[50vh]"
+        >
           <Title title={title} />
 
           <div className="mt-14 w-full">
-            {title == 'about-us' || 'about-mills' && <AboutUs/>}
-            {title == 'how-to-order' && <HowToOrder/>}
-            {title == 'shipping-methods' && <ShippingMethods/>}
-            {title == 'payment-methods' && <PaymentMethods/>}
-            {title == 'faq' && <Faq/>}
-            {title == 'privacy-policy' && <PrivacyPolicy/>}
-            {title == 'exchange-and-return' && <ExchangeAndReturn/>}
-            {title == 'terms-and-conditions' && <ExchangeAndReturn/>}
+            {title == "about-us" && <AboutUs />}
+            {title == "about-mills" && <AboutUs />}
+            {title == "how-to-order" && <HowToOrder />}
+            {title == "shipping-methods" && <ShippingMethods />}
+            {title == "payment-methods" && <PaymentMethods />}
+            {title == "faq" && <Faq />}
+            {title == "privacy-policy" && <PrivacyPolicy />}
+            {title == "exchange-and-return" && <ExchangeAndReturn />}
+            {title == "terms-and-conditions" && <ExchangeAndReturn />}
             <ListSosmed />
           </div>
         </Div>
